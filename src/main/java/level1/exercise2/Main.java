660826319b6e470395ff8e86962cb2a4f8727e1a
Enric <reiben815@gmail.com>
2024-11-21 12:54:37 +0100

    Fix invokeDeprecatedMethod

--- src/main/java/level1/exercise2/Main.java
+++ src/main/java/level1/exercise2/Main.java
@@ -2,15 +2,15 @@ package level1.exercise2;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee1 = new OnlineEmployee("John", "Doe", 10);
-        Employee employee2 = new OnsiteEmployee("Jane", "Doe", 10);
+        OnlineEmployee employee1 = new OnlineEmployee("John", "Doe", 10);
+        OnsiteEmployee employee2 = new OnsiteEmployee("Jane", "Doe", 10);
         Employee employee3 = new Employee("Jack", "Doe", 10);
 
         System.out.println(employee1.calculateSalary(10));
         System.out.println(employee2.calculateSalary(10));
         System.out.println(employee3.calculateSalary(10));
 
-        invokeDeprecatedMethod((OnlineEmployee) employee1, (OnsiteEmployee) employee2);
+        invokeDeprecatedMethod(employee1, employee2);
     }
 
     @SuppressWarnings("deprecation")
